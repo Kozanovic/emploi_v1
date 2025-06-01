96af91acbab1c701aac040c73ba098969772783c
seroking <baridmohamedamine004@gmail.com>
2025-06-01 21:17:55 +0100

    filiere policy

--- app/Policies/FilierePolicy.php
+++ app/Policies/FilierePolicy.php
@@ -16,22 +16,22 @@ class FilierePolicy
     public function create(User $user): bool
     {
         // Seul le DirecteurSuper peut créer une filière
-        return $user->estDirecteurSuper() || $user->estDirecteurRegional();
+        return $user->estDirecteurSuper() ;
     }
     public function update(User $user): bool
     {
-        return $user->estDirecteurSuper() || $user->estDirecteurRegional();
+        return $user->estDirecteurSuper() ;
     }
     public function delete(User $user): bool
     {
-        return $user->estDirecteurSuper() || $user->estDirecteurRegional();
+        return $user->estDirecteurSuper() ;
     }
     public function view(User $user): bool
     {
-        return $user->estDirecteurSuper() || $user->estDirecteurRegional();
+        return $user->estDirecteurSuper() ;
     }
     public function viewAny(User $user): bool
     {
-        return $user->estDirecteurSuper() || $user->estDirecteurRegional();
+        return $user->estDirecteurSuper() ;
     }
 }
